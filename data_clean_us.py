a966108320158bbac581cba7725c967b60df1435
DravitLochan <dlochangupta@gmail.com>
2018-12-12 22:41:15 +0530

    Script for cumulative data

---
+++ data_clean_us.py
@@ -0,0 +1,60 @@
+import csv
+
+filename = "export.csv"
+newfile = "newfile.csv"
+stations = set()
+quarters= set()
+data = {}
+
+def countRows():
+	count = 0
+	with open(filename, 'r') as csvfile:
+		csvreader = csv.reader(csvfile)
+		csvreader.next()
+		for row in csvreader:
+			count = count + 1
+	print(count)
+
+def getStations():
+	with open(filename, 'r') as csvfile:
+		csvreader = csv.reader(csvfile)
+		csvreader.next()
+		for row in csvreader:
+			stations.add(row[2])
+	# for station in stations:
+	# 	print(station)
+	# print(len(stations))	
+
+def getQuarters():
+	with open(filename, 'r') as csvfile:
+		csvreader = csv.reader(csvfile)
+		csvreader.next()
+		for row in csvreader:
+			quarters.add(row[6])
+
+def makeDataCompatible():
+	getStations()
+	getQuarters()
+	with open(filename, 'r') as csvfile:
+		csvreader = csv.reader(csvfile)
+		csvreader.next()
+		for row in csvreader:
+			if row[0] == "Weekday":
+				temp = (row[0], row[2], row[3], row[5])
+			else:
+				temp = ("Weekend", row[2], row[3], row[5])	
+			if temp in data:
+				data[temp] = data[temp] + int(row[6])
+			else:
+				data[temp] = int(row[6])
+	fields = ['DAYSTATUS', 'ENTSTATION', 'EXTSTATION', 'QUARTER', 'AVG_TRIPS']
+	with open(newfile, 'w') as csvfile:
+		writer = csv.writer(csvfile)
+		writer.writerow(fields)
+		for key, value in data.items():
+			temp = list(key)
+			temp.append(value)
+			writer.writerow(temp)
+
+
+makeDataCompatible()
